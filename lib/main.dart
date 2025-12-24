149e6b11f1f5b9709a63e7783079c848d704fda4
Gulomjon Sulaymonov <uzpsychologist@gmail.com>
2025-12-24 14:30:58 +0500

    wd

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -27,38 +28,49 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
-
-class MyWorld extends World {
+class MyWorld extends World with HasGameReference<FlameGame> {
   @override
   Future<void> onLoad() async {
     await super.onLoad();
+
+   var camera = CameraComponent.withFixedResolution(
+      world: MyWorld(),
+      width: 800,
+      height: 600,
+    );
+    // Create parallax using the parent game reference
+    final parallax = await game.loadParallax(
+      [
+        ParallaxImageData('parallax/plx-1.png'),
+        ParallaxImageData('parallax/plx-2.png'),
+        ParallaxImageData('parallax/plx-3.png'),
+        ParallaxImageData('parallax/plx-4.png'),
+        ParallaxImageData('parallax/plx-5.png'),
+        ParallaxImageData('parallax/plx-6.png'),
+      ],
+      baseVelocity: Vector2(20, 0),
+      velocityMultiplierDelta: Vector2(1.8, 1.0),
+    );
+
+    add(ParallaxComponent(parallax: parallax));
+
+    // Character sprite
     final spriteSheet = SpriteSheet(
       image: await Flame.images.load('DinoSprites - tard.png'),
       srcSize: Vector2(24, 24),
     );
-    final image = await Flame.images.load('parallax/plx-1.png');
-    add(ParallaxComponent(
-      parallax: Parallax([
-        ParallaxLayer(
-          ParallaxImage(image, fill: LayerFill.height, alignment: Alignment.bottomLeft, repeat: ImageRepeat.repeat),
-        ),
-      ]),
-    ));
-    final idleAnimation = spriteSheet.createAnimation(row: 0, stepTime: 0.1, from: 0, to: 3);
-    final runAnimation = spriteSheet.createAnimation(row: 0, stepTime: 0.1, from: 3, to: 6);
+
+    final runAnimation = spriteSheet.createAnimation(
+      row: 0,
+      stepTime: 0.1,
+      from: 3,
+      to: 6,
+    );
+
     add(SpriteAnimationComponent(
       animation: runAnimation,
       position: Vector2(100, 120),
       size: Vector2(80, 80),
     ));
-    add(TextComponent(
-      text: 'hello world',
-      textRenderer: TextPaint(
-        style: const TextStyle(color: Colors.white, fontSize: 24),
-      ),
-      position: Vector2(100, 110),
-    ));
   }
 }
-
